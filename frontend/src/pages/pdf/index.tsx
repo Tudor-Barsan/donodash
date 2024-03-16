900f0c494b80a44385a55207cfb6f8811c8eb935
Tudor Barsan <tudor.barsan46@gmail.com>
2024-03-16 14:48:59 -0400

    Final

--- frontend/src/pages/pdf/index.tsx
+++ frontend/src/pages/pdf/index.tsx
@@ -2,17 +2,16 @@ import { useNavigate } from "react-router-dom";
 import { Sidebar } from "../../components/sidebar";
 
 const PDFViewer = (): JSX.Element => {
-
   return (
     <div className="flex bg-gray-50">
-        <aside className="h-screen sticky top-0">
-            <Sidebar />
-        </aside>
-        <img
-            src="/tax.png"
-            alt="card-image"
-            className="h-full w-full object-cover"
-          />
+      <aside className="h-screen sticky top-0">
+        <Sidebar />
+      </aside>
+      <img
+        src="/tax.png"
+        alt="card-image"
+        className="h-full w-full object-cover"
+      />
     </div>
   );
 };
